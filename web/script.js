95d48b6819844599b1eb42247de009ca2d368725
bakapear <proxybomb@gmail.com>
2018-05-21 19:18:42 +0200

    moving stuff

---
+++ web/script.js
@@ -0,0 +1,32 @@
+let ipc = require("electron").ipcRenderer
+window.onload = function () {
+    let input = document.getElementById("field")
+    input.addEventListener("input", function (evt) {
+        ipc.send("onType", escapeRegExp(this.value))
+    })
+
+    input.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 9) {
+            evt.preventDefault()
+            this.focus()
+        }
+    })
+
+    document.getElementById("button").onclick = function () {
+        ipc.send("onButton")
+    }
+}
+function escapeRegExp(str) {
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")
+}
+window.addEventListener("dragover", function (e) {
+    e = e || event
+    e.preventDefault()
+}, false)
+window.addEventListener("drop", function (e) {
+    e = e || event
+    e.preventDefault()
+}, false)
+function click(data) {
+    ipc.send("onClick", data)
+}
