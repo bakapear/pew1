6dc37d6f5c4eef0dd3b4b77645599c083c37beaf
bakapear <proxybomb@gmail.com>
2018-05-24 13:44:03 +0200

    here aswell

--- web/script.js
+++ web/script.js
@@ -1,21 +1,23 @@
 let ipc = require("electron").ipcRenderer
 
+let keys = [">", "#", "?", "/", "="]
+
 window.onload = function () {
     let input = document.getElementById("field")
+    let key = document.getElementById("special")
     input.addEventListener("input", function (evt) {
-        ipc.send("onType", escapeRegExp(this.value))
+        if (keys.includes(this.value[0])) {
+            key.innerHTML = this.value[0]
+            this.value = this.value.substring(1)
+        }
+        ipc.send("onType", [escapeRegExp(this.value), key.innerHTML])
     })
-
     input.addEventListener("keydown", function (evt) {
         if (evt.keyCode === 9) {
             evt.preventDefault()
             this.focus()
         }
     })
-
-    document.getElementById("button").onclick = function () {
-        ipc.send("onButton")
-    }
 }
 
 function escapeRegExp(str) {
